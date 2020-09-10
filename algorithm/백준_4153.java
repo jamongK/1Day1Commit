885d4f4f6b042a541d4a9dd6cc4e67f72c298a6d
jamongK <50661601+jamongK@users.noreply.github.com>
2020-09-10 20:42:01 +0900

    Update 백준_4153.java

--- algorithm/백준_4153.java
+++ algorithm/백준_4153.java
@@ -8,15 +8,16 @@ public class Main {
 		int temp;
 		
 		while (true) {
+			// 변 3개 길이 입력
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			int c = sc.nextInt();
 			
-			int arr[] = {a, b, c};
-			Arrays.sort(arr);
+			int arr[] = {a, b, c}; // 변 3개를 배열 원소로 집어넣음
+			Arrays.sort(arr); // 오름차순 정렬
 			
 			if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
-				break;
+				break; // 0, 0, 0 입력할 경우
 			} else if (Math.pow(arr[2], 2) == Math.pow(arr[0], 2) + Math.pow(arr[1], 2)) {
 				System.out.println("right");
 			} else {
