48fc67ff3f2ec932b896232d9fb0695ccb0049fe
mondue <wislrlxk@gmail.com>
2020-11-26 08:54:01 +0900

    프로그래머스 JadenCase 문자열 만들기 (Java) (split()이용)

---
+++ algorithm/Programmers/JadenCase2.java
@@ -0,0 +1,36 @@
+public class JadenCase2 {
+    public static void main(String[] args) {
+        String s = "hELlo   iT iS  Me   ";
+        JadenCase2 j = new JadenCase2();
+        System.out.println(j.solution(s));
+    }
+
+    public String solution(String s) {
+        String answer = null;
+        StringBuilder sb = new StringBuilder();
+        String[] arr = s.split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!(arr[i].equals(" "))) {
+                if (arr[i].length() > 1) {
+                    sb.append(arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1).toLowerCase());
+                } else {
+                    sb.append(arr[i].toUpperCase());
+                }
+                if (i < (arr.length - 1))
+                    sb.append(" ");
+            } else {
+                sb.append(" ");
+            }
+        }
+
+        int idx = s.length() - 1;
+        while (s.charAt(idx) == ' ') {
+            sb.append(" ");
+            idx--;
+        }
+
+        answer = sb.toString();
+        return answer;
+    }
+}
